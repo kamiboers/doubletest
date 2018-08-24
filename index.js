80fd602e314cb76c291f2a307ed6039e834e4fe5
= <=>
2018-08-24 06:25:09 -0600

    parse multiple entries into decimal strings, linking methods

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 import { mapNumeralsToDigitStrings } from './constants';
 
-export function mapNumeralToDigit (numeralString) {
-  return mapNumeralsToDigitStrings[numeralString];
+export default class AccountNumberParser {
+
+  mapNumeralToDigit (numeralString) {
+    return mapNumeralsToDigitStrings[numeralString];
+  }
+
+  mapNumeralListToDigitString (numeralList) {
+    return numeralList.map( numeral => this.mapNumeralToDigit(numeral) ).join('');
+  }
+
+  parseAccountEntry (accountEntry) {
+    const chars = accountEntry.join('').split('')
+    const newArray = Array(9).fill('');
+
+    accountEntry.forEach(function(n) {
+      for (let i = 0; i < newArray.length; i++) {
+        const splicedChars = chars.splice(0, 3).join('')
+        newArray[i] += splicedChars
+      }
+    })
+    return this.mapNumeralListToDigitString(newArray)
+  }
+
+  parseMultipleAccountEntries (accountEntries) {
+    const entries = [];
+
+    while (accountEntries.length) {
+      entries.push(accountEntries.splice(0, 3))
+    }
+    return entries.map( entry => this.parseAccountEntry(entry) );
+  }
 }
 
-export function mapNumeralListToDigitString (numeralList) {
-  return numeralList.map( numeral => mapNumeralToDigit(numeral) ).join('');
-}
+
+
--- spec/numeral_mapping.spec.js
+++ spec/numeral_mapping.spec.js
@@ -1,16 +1,18 @@
-import { mapNumeralToDigit, mapNumeralListToDigitString } from '../index';
+import AccountNumberParser from '../index';
 import * as num from '../constants';
+
+const Parser = new AccountNumberParser();
 
 test('mapping graphical representation to digit returns correct digit string', () => {
   const four = num.four;
-  expect(mapNumeralToDigit(four)).toEqual('4');
+  expect(Parser.mapNumeralToDigit(four)).toEqual('4');
 });
 
 test('mapping nonexistant numeral to digit returns undefined', () => {
   const nonexistant = "|_|"+
                       "|_|"+
                       "|_|";
-  expect(mapNumeralToDigit(nonexistant)).toEqual(undefined);
+  expect(Parser.mapNumeralToDigit(nonexistant)).toEqual(undefined);
 });
 
 test('mapping list of real numerals to digit returns correct digit string', () => {
@@ -18,20 +20,47 @@ test('mapping list of real numerals to digit returns correct digit string', () =
                            [num.zero], [num.one], [num.two], [num.three], [num.four], 
                            [num.five], [num.six], [num.seven], [num.eight], [num.nine] 
                          ];
-  const result = mapNumeralListToDigitString(listZeroToNine);
+  const result = Parser.mapNumeralListToDigitString(listZeroToNine);
 
   expect(result.length).toEqual(listZeroToNine.length);
   expect(result).toEqual('0123456789');
 });
 
+test('an input of lines is parsed into a list of digits', () => {
+  const oneToNineEntry = [ "    _  _     _  _  _  _  _ ",
+                                    "  | _| _||_||_ |_   ||_||_|",
+                                    "  ||_  _|  | _||_|  ||_| _|",
+                                  ]
+
+  const result = Parser.parseAccountEntry(oneToNineEntry);
+
+  expect(result).toEqual('123456789');
+});
+
+test('multiple entries are parsed into a separate entries of digits', () => {
+  const multipleEntries =        [
+                                    "    _  _     _  _  _  _  _ ",
+                                    "  | _| _||_||_ |_   ||_||_|",
+                                    "  ||_  _|  | _||_|  ||_| _|",
+                                    " _  _  _     _  _     _  _ ",
+                                    "  | _||_||_||_ |_   ||_||_|",
+                                    "  ||_  _|  | _| _|  ||_||_|",
+                                 ]
+
+  const result = Parser.parseMultipleAccountEntries(multipleEntries);
+
+  expect(result.length).toEqual(2);
+  expect(result[0]).toEqual('123456789');
+  expect(result[1]).toEqual('729455188');
+});
+
 
 // parse graphical numerals to digits
 // parse a series of graphical numerals to digits
-
 // parse list of lines into graphical numerals
-
 // parse series of entries (set of lines) into separate collections of numerals
 // print series of entries as decimals
+
 // intake small file, read and output
 // intake large file, read and output
 
